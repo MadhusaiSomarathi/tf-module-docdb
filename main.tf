d069df18493cad3d057c2c751e3899d1168eb501
rthootika <msamrat789@gmail.com>
2023-03-06 13:53:52 +0530

    Updates

--- main.tf
+++ main.tf
@@ -7,20 +7,20 @@ resource "aws_docdb_subnet_group" "default" {
   }
 }
 
-#resource "aws_docdb_cluster" "docdb" {
-#  depends_on             = [aws_docdb_subnet_group.default]
-#  cluster_identifier     = "${var.env}-${var.name}-roboshop-docdb"
-#  engine                 = var.engine
-#  master_username        = "madhu"
-#  master_password        = "Masai789"
-#  skip_final_snapshot    = var.skip_final_snapshot
-#  db_subnet_group_name   = "${var.env}-${var.name}-roboshop-docdb"
-#  vpc_security_group_ids = [aws_security_group.sg.id]
-#}
-#
-#resource "aws_docdb_cluster_instance" "cluster_instances" {
-#  for_each           = var.nodes
-#  identifier         = "${var.env}-${var.name}-roboshop-docdb-${each.key}"
-#  cluster_identifier = aws_docdb_cluster.docdb.id
-#  instance_class     = each.value.instance_class
-#}
+resource "aws_docdb_cluster" "docdb" {
+  depends_on             = [aws_docdb_subnet_group.default]
+  cluster_identifier     = "${var.env}-${var.name}-roboshop-docdb"
+  engine                 = var.engine
+  master_username        = local.DOCDB_USER
+  master_password        = local.DOCDB_PASS
+  skip_final_snapshot    = var.skip_final_snapshot
+  db_subnet_group_name   = "${var.env}-${var.name}-roboshop-docdb"
+  vpc_security_group_ids = [aws_security_group.sg.id]
+}
+
+resource "aws_docdb_cluster_instance" "cluster_instances" {
+  for_each           = var.nodes
+  identifier         = "${var.env}-${var.name}-roboshop-docdb-${each.key}"
+  cluster_identifier = aws_docdb_cluster.docdb.id
+  instance_class     = each.value.instance_class
+}
--- outputs.tf
+++
@@ -1,3 +0,0 @@
-output "secrets" {
-  value = jsondecode(data.aws_secretsmanager_secret_version.roboshop.secret_string)
-}
